918cf21483ec1c063c9bb4dc957b4a6881ba476b
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-09 15:34:14 -0700

    check if number is positive, zero or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,8 @@
 
 /*
  * main - check if number is positive, zero or negative
- * 
  * Description: using the main function
- * this program prints 'Programming is positive, zero, or negative'
+ * this program prints 'Programming is positive, zero, or negative
  * Return (0)
  */
 int main(void)
