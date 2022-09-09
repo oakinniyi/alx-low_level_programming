30ced8880394b1cbee9a42489899b38b0c2c6bdc
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-09 16:46:01 -0700

    prints alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lower case
+ *
+ * Description: using the main function
+ * prints alphabets in lower case, followed be a new line
+ * Return: 0;
+ */
+int main(void)
+{
+	char apl[26] = "a..z";
+	int i;
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n);
+	return (0);
+}
