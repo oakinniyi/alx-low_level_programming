67d7c6d9cdcce368494feebf40fc9c1acffc3a8b
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-09 14:54:42 -0700

    check if number is positive, zero or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* main - check if number is positive, zero or negative
+ *
+ * Return (0)
+ *
+ */
+int main(void)
+{
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else (n < 0)
+	{
+		printf("%d is negative\n", n)
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+The aim of this project is to learn arithmetic operators, boolean operators, relational operators, comments, variables and oops in C programming language
+
