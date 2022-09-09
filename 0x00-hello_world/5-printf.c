c29638fda0b599d0ff8f9bfc22e58ce500a4531f
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-08 18:00:52 -0700

    I will overcome

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Description: using main function
-* This program prints with proper grammer but the outcome is a piece of art
-* Return: 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
- 	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
