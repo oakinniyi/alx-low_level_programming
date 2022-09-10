0b6582acd39f50f6b3b346e7178f3b42503ce35f
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-09 19:30:04 -0700

    putting commas intween figures

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Description: using the main function
+ * this program prints 0 to 9 with commas inbetween them
+ * Return: 0;
+ */
+int main(void)
+{
+	int i;
+
+	for (i =48; i <= 57; i++)
+	{
+		putchar(i);
+		if (c != 57)
+		{
+			putchar(',')
+			putchar(' ')
+		}
+	}
+	putchar('\n');
+	return (0);
+}
