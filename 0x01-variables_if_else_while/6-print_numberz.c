d8c22feaf4ebe1ba7071a8dade1d0e9270f01686
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-09 18:18:59 -0700

    print digits from 0 to 9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,11 @@
  * prints numbers ranging from 0 to 9
  * Return: 0;
  */
-int main (void)
+int main(void)
 {
 	int i;
 
-	for (i = 4; i < 58; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
