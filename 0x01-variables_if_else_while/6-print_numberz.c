3cf549ef01613fbffdd3c1f1ab5707e31485ccac
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-09 18:15:13 -0700

    print digits from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 9
+ *
+ * Description: using the main function
+ * prints numbers ranging from 0 to 9
+ * Return: 0;
+ */
+int main (void)
+{
+	int i;
+
+	for (i = 48, i < 58, i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
