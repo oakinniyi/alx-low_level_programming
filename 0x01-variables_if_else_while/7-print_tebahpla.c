d62384a87cd5e1b80262e3ac613e9ab580267dc7
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-09 18:28:32 -0700

    letters in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - prints alphabets in lower case and in reverse
+ *
+ * Description: using the main function
+ * prints alphabets in lower case and in reverse
+ * Return: 0;
+ */
+int main(void)
+{
+	int i
+	
+	for (i = 122; i > 96; i++)
+	{
+		putchar(i)
+	}
+	putchar('\n');
+	return (0);
+}
