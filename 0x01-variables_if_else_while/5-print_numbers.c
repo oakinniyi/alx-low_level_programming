83cda410abf4e20dce53e94b8764d0285ba82f72
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-09 17:41:04 -0700

    print figure 1 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits 1 to 9
+ *
+ * Description: using the main function
+ * prints 1 to 9
+ * Return: 0;
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 0; 1 < 10, i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
