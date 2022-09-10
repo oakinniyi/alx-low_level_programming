9dabff2d981e792e16eb6ffc5a1a34b8b82f956a
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-09 17:21:11 -0700

    excluding q and e from letters to be printed

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - prints alphabets in lower case except q and e
+ *
+ * Description: using the main function
+ * prints all alphabets in lower case and ignoring q and e
+ * Return: 0;
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			purchar(i);
+		}
+	}
+	putchar('\n')
+	return (0);
+}
