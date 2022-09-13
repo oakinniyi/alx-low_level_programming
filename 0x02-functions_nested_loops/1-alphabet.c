b2167622f22acef01a529689a5b499c0c85d0396
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-13 11:05:57 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** print_alphabets - this fubction prints alphabets
+ *
+ * Description: the funtion above prints all alphabets in lower case
+ *
+ * Return: 0
+ */
+void print_alphabets(void)
+{
+	char c
+	
+	for (c = 'a'; c <= z; c++);
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
