10ad217a8553dc50db0d3730d97b48ac2171286e
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-13 13:19:15 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * islower - checks for lowercase character
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int _islower(int c)
+	{
+		if (i >= 'a' && i <= 'z')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
