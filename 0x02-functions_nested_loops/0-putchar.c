abe4cb0488bff1583664876f7f5a753dbe7cd321
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-13 07:05:00 -0700

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/** main - This program prints _putchar, followed by a new line
+ *
+ * Description: using the main function
+ * this program prints _putchar, followed by a new line
+ * Result: 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
