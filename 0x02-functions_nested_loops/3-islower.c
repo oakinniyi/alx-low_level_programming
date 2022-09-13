da9ba8bd4b3e767cc265fe77ef8a94aa5dff1b06
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-13 13:27:38 -0700

    lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,17 +5,14 @@
  *
  * Return: Always 0
  */
-int main(void)
+int _islower(int c)
 {
-	int _islower(int c)
+	if (c >= 'a' && c <= 'z')
 	{
-		if (c >= 'a' && c <= 'z')
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 }
