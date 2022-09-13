81a86fa398044440ed01414739e86436286babc5
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-13 14:36:57 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @c: is the int that is used for the argument of the function
+ *
+ * Return: Always 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
