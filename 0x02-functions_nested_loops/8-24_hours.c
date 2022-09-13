374ce4c01bc7c423425002c95e5d0a81bf4983a8
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-13 15:22:46 -0700

    jack

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * jack_baue - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
+ * @n: is the argument of the function
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int n;
+
+	00:00 <= n && n <= 23:59
+	if (n = 0;; n+0.10)
+	{
+		_putchar('n');
+	}
+	_putchar('\n');
+}
