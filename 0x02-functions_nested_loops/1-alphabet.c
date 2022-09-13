25b4adf23a13c9af5cc531e9a36f6df1aab7727b
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-13 11:31:24 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-/** print_alphabets - this fubction prints alphabets
+/**
+ *print_alphabets - this fubction prints alphabets
  *
  * Description: the funtion above prints all alphabets in lower case
- *
  * Return: 0
  */
 void print_alphabet(void)
