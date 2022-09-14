a8e5e64dd646601743c36ae0cdcf987657b3633d
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-14 14:45:31 -0700

    Fibonacci numbers2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even-valued terms
+ * Return: 0
+ */
+int main(void)
+{
+	long int a1, a2, fa, b;
+
+	a1 = 1;
+	a2 = 2;
+	fa = b = 0;
+	while (fn <= 4000000)
+	{
+		fa = a1 + a2;
+		a1 = a2;
+		a2 = fa;
+		if ((a1 % 2) == 0)
+		{
+			b += n1;
+		}
+	}
+	printf("%ld\n", b);
+	return (0);
+}
