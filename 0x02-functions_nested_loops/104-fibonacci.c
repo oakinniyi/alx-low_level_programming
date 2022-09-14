debc384f97d15de7c8bd6f7b2d6f11d0eaa2283e
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-14 15:01:33 -0700

    Fibonacci numbers3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the first 98 Fibonacci numbers, starting with 1 and 2
+ * main - finds and prints the first 98 Fibonacci
+ * numbers, starting with 1 and 2
  * Return: 0
  */
 int main(void)
