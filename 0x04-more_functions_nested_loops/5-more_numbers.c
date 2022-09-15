2781817d779858447429e2d36e4f50dd9da2693b
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-15 14:29:52 -0700

    more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9, i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			for (j >= 10)
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
