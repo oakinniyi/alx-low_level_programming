837132823914c747a4a37cd42e68d649ae422880
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-15 17:51:29 -0700

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,14 +9,15 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		for (j = 0; j < size; j++)
+		for (i = 0; i < size; i++)
 		{
-			_putchar(35)
+			for (j = 0; j < size; j++)
+				_putchar(35);
+				_putchar('\n');
 		}
-		if (i != size - 1)
-			_putchar('\n');
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
