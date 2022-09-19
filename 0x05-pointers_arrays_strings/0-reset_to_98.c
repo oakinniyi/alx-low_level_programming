a21fb63bada34057eb854fccd259e13f319a8dd3
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-19 19:59:00 +0000

    reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value
+ * @n: is an integer input
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
