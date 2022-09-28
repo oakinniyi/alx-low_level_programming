788364a52f232e04352ea6a0fe3c59a2dd2adb74
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-09-28 14:23:48 -0700

    recursion 5

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,24 +2,22 @@
 
 /**
  * _pow_recursion - returns the value of x raised to the power of y
- * @n: postive integer
+ * @x: postive integer
  * @y: power of n
  * Return: x to the power of y
  */
 int _pow_recursion(int x, int y)
 {
-	int i = 0, a;
-	
 	if (y < 0)
 	{
-	    return (-1);
-    }
-    else if (y == 0)
-    {
-        return (1);
-    }
-    else
-    {
-        return (x * _pow_recursion(x, y - 1));
-    }
+		return (-1);
+	}
+		else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
 }
