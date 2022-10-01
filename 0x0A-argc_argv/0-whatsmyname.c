7c677f8b1684e3ddc4328e8a54091bc9f78c2ea2
Olufemi John Akinniyi <akinniyiolufemi89@gmail.com>
2022-10-01 10:47:50 -0700

    argcargv2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: array that contains the program command line arguments
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
